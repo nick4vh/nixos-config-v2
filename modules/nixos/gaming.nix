ae9a85a2e6556ab466465b31772d0d774e4eca92
nick4vh <nick@viereggeheyng.de>
2025-05-17 22:06:04 +0200

    edit gaming config

--- modules/nixos/gaming.nix
+++ modules/nixos/gaming.nix
@@ -2,66 +2,196 @@
 { config, pkgs, lib, ... }:
 
 {
-  # Vulkan Unterstützung (bereits in common.nix und hardware Modulen, hier zur Verdeutlichung)
-  hardware.opengl.enable = true;
-
-  # Pakete für Gaming (einige sind besser als User-Pakete, aber hier als Systemoption für Bibliotheken)
-  environment.systemPackages = with pkgs; [
-    vulkan-tools # Für `vulkaninfo` und Tests
-    vulkan-loader
-    # mesa.drivers # Mesa-Treiber (werden durch hardware.opengl.enable bereitgestellt)
-    # Für NVIDIA: nvidia-vaapi-driver für VA-API über NVIDIA (experimentell)
-    # libva # VA-API Implementierung
-
-    # Wine und verwandte Pakete (oft besser per Home Manager für den Benutzer `nick`)
-    # wineWowPackages.stable # Enthält 64-bit und 32-bit Wine
-    # winetricks
-    # dxvk # DX9/10/11 auf Vulkan
-    # vkd3d-proton # DX12 auf Vulkan
-
-    # Gamescope für Wayland/X11 Compositing und Upscaling von Spielen
-    gamescope
-
-    # MangoHud für Leistungsüberwachung in Spielen
-    mangohud
-  ];
-
-  # Kernel-Optimierungen (optional, Standard-Kernel ist oft gut genug)
-  # boot.kernelPackages = pkgs.linuxPackages_zen; # Zen-Kernel
-  # boot.kernelPackages = pkgs.linuxPackages_xanmod; # XanMod-Kernel
-  # Wenn ein spezieller Kernel verwendet wird, sicherstellen, dass externe Module (NVIDIA) kompatibel sind.
-  # NixOS handhabt dies meist gut.
-
-  # Controller Support
-  # Grundlegende Joystick-Unterstützung ist normalerweise im Kernel enthalten.
-  # Spezifische Controller-Unterstützung kann über udev-Regeln oder Pakete hinzugefügt werden.
-  services.udev.packages = with pkgs; [
-    # Pakete für spezifische Controller-Unterstützung, falls nötig:
-    # z.B. `xone-dkms` (für moderne Xbox Controller, erfordert Kernel-Header)
-    # `ds4drv` (für DualShock 4, oft nicht mehr nötig, da Kernel-Support gut ist)
-    # Steam selbst bringt oft schon gute Controller-Unterstützung mit (Steam Input).
-  ];
-
-  # FUSE für Proton/Steam (AppImage, etc.)
-  # Wird oft automatisch durch `programs.steam.enable = true` im Home Manager oder systemweit gehandhabt.
-  # Falls nicht:
-  # users.users.nick.extraGroups = [ "fuse" ]; # Erlaubt das Mounten von FUSE Dateisystemen
-  # Oder systemweit:
-  # environment.systemPackages = [ pkgs.fuse ];
-
-  # Gamemode für Performance-Optimierungen beim Spielen
-  programs.gamemode.enable = true;
-
-  # MangoHud systemweit aktivieren (optional, kann auch pro Spiel via Steam gestartet werden)
-  # programs.mangohud.enable = true; # Wird bereits in environment.systemPackages hinzugefügt
-  # programs.mangohud.enableSessionWide = true; # Lädt MangoHud für alle Anwendungen (zum Testen)
-
-  # PipeWire für Audio (bereits in audio.nix konfiguriert)
-  # services.pipewire.enable = true;
-  # services.pipewire.alsa.support32Bit = true; # Wichtig für 32-Bit Spiele
-
-  # Spezielle Einstellungen für Steam
-  programs.steam = {
-    enable = true; # Systemweite Installation von Steam (oft besser per Home Manager)
+  imports = [];
+  
+  nixpkgs.config = {
+    allowUnfree = true;
+    # Wichtig für Steam und manche Spiele
+    packageOverrides = pkgs: {
+      steam = pkgs.steam.override {
+        extraPkgs = pkgs: with pkgs; [
+          libgdiplus
+          keyutils
+          libkrb5
+          libpng
+          libpulseaudio
+          libvorbis
+          stdenv.cc.cc.lib
+          xorg.libXcursor
+          xorg.libXi
+          xorg.libXinerama
+          xorg.libXScrnSaver
+          at-spi2-atk
+          at-spi2-core
+          gtk3
+          glib
+          pango
+          gdk-pixbuf
+          cairo
+          atk
+          zlib
+          glibc
+          openssl
+        ];
+      };
+    };
   };
+
+  environment.systemPackages = with pkgs; [
+    # Gaming-Plattformen
+    steam
+    lutris
+    heroic
+    bottles
+    mangohud
+    goverlay
+    gamemode
+    
+    # Tools für Windows-Kompatibilität / Proton
+    wineWowPackages.staging
+    winetricks
+    protonup-qt
+    
+    # Controller-Support
+    antimicrox
+    
+    # Tools und Abhängigkeiten für bessere Kompatibilität
+    protontricks
+    steam-run
+    glxinfo
+    pciutils
+    
+    # Bibliotheken, die für viele Spiele benötigt werden
+    xorg.libXcomposite
+    xorg.libXtst
+    xorg.libXrandr
+    xorg.libXext
+    xorg.libX11
+    xorg.libXfixes
+    libGL
+    libva
+    
+    # Audio-Bibliotheken
+    alsa-lib
+    alsa-plugins
+    libpulseaudio
+    
+    # Für Counter-Strike: Source spezifisch hilfreich
+    gnutls
+    openal
+    libvdpau
+    
+  ] ++ (with pkgs.pkgsi686Linux; [
+    libpulseaudio
+    libGL
+    vulkan-loader
+    libvdpau
+    alsa-lib
+    alsa-plugins
+    libpng
+    libgpg-error
+    gnutls
+    openal
+  ]);
+
+  # Gamemode aktivieren für bessere Spielleistung
+  programs.gamemode = {
+    enable = true;
+    settings = {
+      general = {
+        softrealtime = "auto";
+        renice = 10;
+      };
+      gpu = {
+        apply_gpu_optimisations = "accept-responsibility";
+        gpu_device = 0;
+      };
+      custom = {
+        start = "${pkgs.libnotify}/bin/notify-send 'GameMode aktiviert'";
+        end = "${pkgs.libnotify}/bin/notify-send 'GameMode deaktiviert'";
+      };
+    };
+  };
+
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true;
+    dedicatedServer.openFirewall = true;
+    gamescopeSession.enable = true;  # Optionaler Gamescope-Support für bessere Performance
+  };
+
+  # Vulkan Support
+  hardware.opengl = {
+    enable = true;
+    # driSupport = true;  # Diese Option ist jetzt standardmäßig aktiviert
+    driSupport32Bit = true;  # Diese Option ist noch wichtig für 32-Bit-Spiele
+    extraPackages = with pkgs; [
+      vulkan-loader
+      vulkan-tools
+      vulkan-validation-layers
+      vaapiVdpau
+      libvdpau-va-gl
+    ];
+    extraPackages32 = with pkgs.pkgsi686Linux; [
+      vulkan-loader
+      libva
+      vaapiVdpau
+    ];
+  };
+
+  # Ersetzen von hardware.graphics durch hardware.opengl
+  # hardware.graphics ist veraltet
+  
+  hardware.steam-hardware.enable = true;
+  
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    jack.enable = true;  # Für Audioanwendungen, die JACK verwenden
+  };
+
+  services.udev.packages = with pkgs; [ steam ];
+  
+  # Erweiterte Firewall-Regeln für Steam
+  networking.firewall = {
+    allowedTCPPorts = [ 27036 27037 ];
+    allowedUDPPorts = [ 27031 27036 27037 ];
+    # Zusätzliche In-Home-Streaming-Ports
+    allowedTCPPortRanges = [ { from = 27015; to = 27050; } ];
+    allowedUDPPortRanges = [ { from = 27000; to = 27100; } ];
+  };
+
+  services.flatpak.enable = true;
+  
+  # Environment-Variablen für Steam und Proton
+  environment.sessionVariables = {
+    STEAM_EXTRA_COMPAT_TOOLS_PATHS = "~/.steam/root/compatibilitytools.d";
+    # Hilft bei Problemen mit OpenGL in einigen Spielen
+    LIBGL_DRI3_DISABLE = "1";
+    # Hilft bei manchen Source-Engine-Spielen
+    __GL_THREADED_OPTIMIZATIONS = "1";
+    # Verbesserung der Nvidia-Treiber-Performance (falls verwendet)
+    __GL_SHADER_DISK_CACHE = "1";
+    # Deaktiviert Async-Shader-Kompilation, die bei CS:S zu Abstürzen führen kann
+    __GL_SHADER_DISK_CACHE_SKIP_CLEANUP = "1";
+  };
+
+  # ACO Compiler für AMD-GPUs aktivieren (falls vorhanden)
+  environment.variables = lib.mkIf (config.services.xserver.videoDrivers != null && 
+                               builtins.elem "amdgpu" config.services.xserver.videoDrivers) {
+    RADV_PERFTEST = "aco";
+  };
+
+  # Die folgenden Core-Limits verbessern die Stabilität von Spielen
+  security.pam.loginLimits = [
+    { domain = "@users"; item = "rtprio"; type = "-"; value = 98; }
+    { domain = "@users"; item = "memlock"; type = "-"; value = "unlimited"; }
+    { domain = "@users"; item = "nofile"; type = "soft"; value = 8192; }
+  ];
+
+  # Für USB-Controller wichtig
+  hardware.xpadneo.enable = true;
+  hardware.xone.enable = true;
 }
